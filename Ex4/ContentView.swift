bacad5fbc064b115cd43dcc14e347cb405aa02ce
Dobrovsky <miron.dobrovsky.work@gmail.com>
2024-07-26 17:57:04 +0300

    Initial Commit

---
+++ Ex4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ex4
+//
+//  Created by Dobrovsky on 26.07.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ex4/Ex4App.swift
@@ -0,0 +1,17 @@
+//
+//  Ex4App.swift
+//  Ex4
+//
+//  Created by Dobrovsky on 26.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct Ex4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
